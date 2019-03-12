14a921a1ec07cbd89d5649d816c3760aac3b3d22
Zach Layman <zachlayman@Zachs-MacBook-Pro.local>
2019-03-12 15:45:57 -0700

    add cbt test

---
+++ src/test/java/com/applitools/demos/CBTTests.java
@@ -0,0 +1,53 @@
+package com.applitools.demos;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.applitools.eyes.BatchInfo;
+import com.applitools.eyes.MatchLevel;
+import com.applitools.eyes.selenium.Eyes;
+import com.applitools.eyes.selenium.config.Configuration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class CBTTests extends BaseTest {
+    private DesiredCapabilities dc;
+    String cbtURL;
+
+    @Override
+    @BeforeClass
+    public void setupEyes() {
+        batchInfo = new BatchInfo(batchName);
+
+        eyes = new Eyes();
+        eyes.setApiKey(System.getenv("APPLITOOLS_API_KEY"));
+        eyes.setBatch(batchInfo);
+        eyes.setMatchLevel(MatchLevel.STRICT);
+
+        config = new Configuration();
+        config.setAppName("Zach's Demo Java App");
+        
+        dc = new DesiredCapabilities();
+        dc.setCapability("browserName", "Chrome");
+        dc.setCapability("deviceName", "Galaxy S7");
+        dc.setCapability("platformVersion", "7.0");
+        dc.setCapability("platformName", "Android");
+        dc.setCapability("deviceOrientation", "portrait");
+
+        cbtURL = String.format(
+           "http://%s:%s@hub.crossbrowsertesting.com:80/wd/hub",
+           System.getenv("CBTUSRNAME").replaceAll("@", "%40"), System.getenv("CBTAUTH"));
+    }
+
+    @Test
+    public void cbtPage() throws MalformedURLException {
+        driver = new RemoteWebDriver(new URL(cbtURL), dc);
+        config.setTestName("CBT Test");
+
+        snapWebpage("https://ehp-prod.literatumonline.com", By.id("logo"), null);
+    }
+}

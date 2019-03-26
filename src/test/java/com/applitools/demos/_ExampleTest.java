d34e697a215f226aa0d8c05b2dafa57833f6ee7c
Zach Layman <zachlayman@Zachs-MacBook-Pro.local>
2019-03-26 14:20:26 -0700

    add example test file

---
+++ src/test/java/com/applitools/demos/_ExampleTest.java
@@ -0,0 +1,24 @@
+package com.applitools.demos;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class _ExampleTest extends BaseTest {
+
+    @Override
+    @BeforeClass
+    public void setupEyes() {
+        batchName = "_ExampleTest";
+        super.setupEyes();
+    }
+
+    @Test
+    public void chrome() {
+        driver = new ChromeDriver();
+        config.setTestName("_Example Test");
+
+        snapWebpage("http://google.com", By.id("hplogo"), null);
+    }
+}

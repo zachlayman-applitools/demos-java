c4016aefd3657f72be5cdf7d81a223e6cc5f5467
Zach Layman <zachlayman@Zachs-MacBook-Pro.local>
2019-04-08 16:22:59 -0700

    trying layout in strict, fluent

---
+++ src/test/java/com/applitools/demos/LayoutInsideStrictTest.java
@@ -0,0 +1,33 @@
+package com.applitools.demos;
+
+import com.applitools.eyes.MatchLevel;
+import com.applitools.eyes.selenium.fluent.Target;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class LayoutInsideStrictTest extends BaseTest {
+
+    @Override
+    @BeforeClass
+    public void setupEyes() {
+        batchName = "Layout Inside Strict";
+        super.setupEyes();
+    }
+
+    @Test
+    public void layoutInsideStrict() {
+        driver = new FirefoxDriver();
+        config.setTestName("Layout Inside Strict");
+        
+        eyes.setMatchLevel(MatchLevel.STRICT);
+
+        eyes.open(driver, config);
+        driver.get("https://applitools.com/helloworld?diff1");
+        waitForIsDisplayed(driver, By.className("section"), timeout);
+
+        eyes.check(Target.window().layout());
+    }
+}

bb77896c311ecec4bcc39231c66823c461cd2eec
Zach Layman <zachlayman@Zachs-MacBook-Pro.local>
2019-05-22 15:27:05 -0700

    add to hover issue w tooltip example

--- src/test/java/com/applitools/demos/issues/I_28822_HoverElement.java
+++ src/test/java/com/applitools/demos/issues/I_28822_HoverElement.java
@@ -4,6 +4,7 @@ import com.applitools.demos.BaseTest;
 import com.applitools.eyes.selenium.fluent.Target;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.BeforeClass;
@@ -19,7 +20,7 @@ public class I_28822_HoverElement extends BaseTest {
     }
 
     @Test
-    public void chrome() {
+    public void hoverButton() {
         driver = new FirefoxDriver();
         driver.get("https://coxkit-demo.homenetauto-np.cloud/");
         driver.findElement(By.id("core-clickableComponents")).click();
@@ -34,6 +35,25 @@ public class I_28822_HoverElement extends BaseTest {
         Actions a = new Actions(driver);
         a.moveToElement(driver.findElement(By.id("primaryButton"))).perform();
         eyes.check("after button", Target.region(By.id("buttonExamples")));
+    }
 
+    @Test
+    public void captureTooltip() {
+        driver = new ChromeDriver();
+        driver.get("https://d1q3vzvnjy4w20.cloudfront.net/");
+        driver.findElement(By.id("coxkit-tooltip")).click();
+
+        config.setTestName("I_28822_HoverElement_Tooltip");
+        config.setForceFullPageScreenshot(true);
+        eyes.setConfiguration(config);
+
+        eyes.open(driver);
+
+        eyes.check("before hover", Target.window());
+
+        Actions action = new Actions(driver);
+        action.moveToElement(driver.findElement(By.id("warningHover"))).perform();
+
+        eyes.checkElement(By.cssSelector("div[content ^= 'I want to tell you']"), "Le Tooltip");
     }
 }
